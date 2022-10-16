3017032c1e4298ac76468ecbaa036019e0ef8eae
Liuyushiii <liuyushi001@163.com>
2022-10-16 03:01:59 -0700

    20221016

--- cmd.sh
+++ cmd.sh
@@ -2,4 +2,5 @@ cargo run -p graph-node --release -- \
   --postgres-url postgresql://postgres:postgres@localhost:5432/graphnode \
   --nebula-url root:root@localhost:9669/testGraph \
   --ethereum-rpc mainnet:http://localhost:7545 \
-  --ipfs 127.0.0.1:5001 --debug
+  --ipfs 127.0.0.1:5001 --debug \
+  >> log
